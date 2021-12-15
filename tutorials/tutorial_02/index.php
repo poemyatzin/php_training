88673516d03816f025a7f538ee885491efad7527
poemyatzin <poemyatzin1995@gmail.com>
2021-12-15 12:27:02 +0630

    [master] finished php tutorial 2

--- tutorials/tutorial_02/index.php
+++ tutorials/tutorial_02/index.php
@@ -1,45 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tutorial_01_chessboard</title>
- <style>
-     table{
-         width: 400px;
-         height:400px;
-         border:1px solid black;
-     }
-     .even{
-         background-color:white;
-         color:black;
-     }
-     .odd{
-        background-color:black;
-     }
- </style>
-</head>
-<body>
-    <table>
-    <?php
-    for($i=1;$i<=8;$i++)
-    {
-        echo"<tr>";
-        for($j=1;$j<=8;$j++)
-        {
-            if(($i+$j)%2==0)
-            {
-                echo"<td class='even'></td>";
-            }
-            else{
-                echo"<td class='odd'></td>";
-            }
-
-        }
-        echo"</tr>";
-    }
-    ?>
-    </table>
-</body>
-</html>
+<?php
+$x=9;
+$sa=1;
+for($i=1;$i<=11;$i++)
+{
+	if($i<=6)
+	{
+	for($s=6;$s>=$i;$s--)
+	{
+		echo "&nbsp; ";
+	}
+	$v=($i*2)-1;
+	for($s=1;$s<=$v;$s++)
+	{
+		echo "*&nbsp;";
+	}
+	echo "<br>";
+	}
+	elseif ($i>6) {
+	for($s=0;$s<=$sa;$s++)
+	{
+		echo "&nbsp; ";
+	}
+	for($s=$x;$s>=1;$s--)
+	{
+		echo "*&nbsp;";
+	}
+	echo "<br>";
+	$x-=2;
+	$sa++;
+	}
+}
+?>
