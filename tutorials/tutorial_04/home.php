599072f58486b4d989aa7ea67b536b44eed3954f
poemyatzin <poemyatzin1995@gmail.com>
2021-12-16 13:52:03 +0630

    [master] finished php tutorial 4

---
+++ tutorials/tutorial_04/home.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial_4 Login Page</title>
+    <style>
+</style>
+</head>
+<body>
+    <form action="login.php">
+    <?php
+session_start();
+echo "<h2>This is Home Page</h2>";
+echo "This is your username " .$_SESSION['uu']."<br>";
+session_destroy();
+?>
+    <input type="submit" name="btnlogout" value="Logout">
+    </form>
+   
+</body>
+</html>
