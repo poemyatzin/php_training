a38ba8754c6d6f6be1d7b1a0c9588ac9a055fc67
poemyatzin <poemyatzin1995@gmail.com>
2021-12-16 13:52:27 +0630

    [master] finished php tutorial 4

---
+++ tutorials/tutorial_04/login.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutorial_4 Login Page</title>
+    <style>
+    table{
+        margin:0 auto;
+        width:300px; 
+    }
+    h1{
+        text-align:center;
+    }
+</style>
+</head>
+<body>
+    <h1>Login Page</h1>
+    <table>
+        <form action="home.php" method="post">
+            <tr>
+                <td>
+                    <label>User Name</label>
+                </td>
+                <td><input type="text" name="txtusername"></td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Password</label>
+                </td>
+                <td><input type="password" name="txtpwd"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="btnsubmit" value="Login"></td>
+            </tr>
+        </form>
+    </table>
+    <?php
+    $_SESSION['uu'];
+    session_start();
+    $uname="admin";
+    $pwd="admin";
+    if(isset($_POST['btnsubmit'])){
+        if($_POST['txtusername'] ==$uname && $_POST['txtpwd'] ==$pwd ){
+          $_SESSION['uu']=$uname;
+    }
+    else{
+        echo "Check your username & Password ";
+    }
+}
+?>
+    </body>
+</html>
+   
