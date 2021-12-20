8b75f98c86ffdf61a2ab668cd2d8b12d83608f8a
poemyatzin <poemyatzin1995@gmail.com>
2021-12-20 16:07:33 +0630

    [master] update variable&comment in tutorial_02

--- tutorials/tutorial_02/index.php
+++ tutorials/tutorial_02/index.php
@@ -1,25 +1,25 @@
 <?php
-$x = 9;
-$sa = 1;
+$x = 9; //star for second part
+$sa = 1;// space for second part
 for ($i = 1; $i <= 11; $i++) {
 	if ($i <= 6) {
-		for ($s = 6; $s >= $i; $s--) {
+		for ($space = 6; $space >= $i; $space--) {
 			echo "&nbsp; ";
 		}
 		$v = ($i * 2) - 1;
-		for ($s = 1; $s <= $v; $s++) {
+		for ($star = 1; $star <= $v; $star++) {
 			echo "*&nbsp;";
 		}
 		echo "<br>";
-	} elseif ($i > 6) {
-		for ($s = 0; $s <= $sa; $s++) {
+	} elseif ($i > 6) { //second part
+		for ($space = 0; $space <= $sa; $space++) {
 			echo "&nbsp; ";
 		}
-		for ($s = $x; $s >= 1; $s--) {
+		for ($star = $x; $star >= 1; $star--) {
 			echo "*&nbsp;";
 		}
 		echo "<br>";
-		$x -= 2;
-		$sa++;
+		$x -= 2; // decrement star for each line
+		$sa++; //increment space for each line
 	}
 }
